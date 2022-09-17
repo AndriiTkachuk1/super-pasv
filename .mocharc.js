0ca679fb8cd42a8a63bf1ea6fc95906664ef0550
Andrii Tkachuk <andreybounce@gmail.com>
2022-09-17 10:35:29 -0500

    add before hooks for auth spec add rood before hook

--- .mocharc.js
+++ .mocharc.js
@@ -1,9 +1,6 @@
 module.exports = {
     require: '@babel/register',
-<<<<<<< HEAD
-    spec: 'specs/**/*.spec.js'
-=======
     spec: 'specs/**/*.spec.js',
     ignore: 'specs/example.spec.js',
->>>>>>> origin/first-api-tests
+    file: 'config/setup.js'
 }
